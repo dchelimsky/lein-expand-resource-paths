f46982effc625ad8ce27a7a594f8f0794ebb02fb
David Chelimsky <dchelimsky@gmail.com>
2013-05-12 11:38:10 -0500

    Revert "remove specific clojure dep"
    This reverts commit 046de886aa663e741c23d7b7b13857aa2225d6e6.

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/dchelimsky/lein-expand-resource-paths"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[clj-glob "1.0.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-glob "1.0.0"]])
