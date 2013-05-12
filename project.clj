046de886aa663e741c23d7b7b13857aa2225d6e6
David Chelimsky <dchelimsky@gmail.com>
2013-05-12 11:33:57 -0500

    remove specific clojure dep

--- project.clj
+++ project.clj
@@ -3,5 +3,4 @@
   :url "https://github.com/dchelimsky/lein-expand-resource-paths"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-glob "1.0.0"]])
+  :dependencies [[clj-glob "1.0.0"]])
