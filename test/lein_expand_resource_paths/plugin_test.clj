dd00b5bd8d6479576a57b1181f707fb355071b46
David Chelimsky <dchelimsky@gmail.com>
2013-05-12 22:07:23 -0500

    more tests

--- test/lein_expand_resource_paths/plugin_test.clj
+++ test/lein_expand_resource_paths/plugin_test.clj
@@ -4,6 +4,8 @@
 
 (deftest expand-paths
   (is (= {:resource-paths []} (middleware {:resource-paths ["i-do-not-exist/*"]})))
+  (is (= {:resource-paths ["./test/resources/a/1.txt"]} (middleware {:resource-paths ["test/resources/a/1.txt"]})))
   (is (= {:resource-paths ["./test/resources/a/1.txt" "./test/resources/a/2.txt"]} (middleware {:resource-paths ["test/resources/a/*"]})))
   (is (= {:resource-paths ["./test/resources/b/3.txt" "./test/resources/b/4.txt"]} (middleware {:resource-paths ["test/resources/b/*"]})))
+  (is (= {:resource-paths ["./test/resources/a/1.txt" "./test/resources/a/2.txt" "./test/resources/b/3.txt"]} (middleware {:resource-paths ["test/resources/a/*" "test/resources/b/3.txt"]})))
   (is (= {:resource-paths ["./test/resources/a/1.txt" "./test/resources/a/2.txt" "./test/resources/b/3.txt" "./test/resources/b/4.txt"]} (middleware {:resource-paths ["test/resources/**/*"]}))))
